2620308a04d919562c7d25982f605ffe38b8e916
Adam McElroy <adam.mcelroy15@gmail.com>
2026-02-05 20:29:35 +0000

    Walking skeleton for client side in React

--- API/Program.cs
+++ API/Program.cs
@@ -10,8 +10,14 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+app.UseCors(x => 
+    x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000"));
 
 app.MapControllers();
 
