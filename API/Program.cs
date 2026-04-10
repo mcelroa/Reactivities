2008e59f3fb0ee4e4ab45286dab11e9204ac7f2f
Adam McElroy <adam.mcelroy15@gmail.com>
2026-04-10 17:38:23 +0100

    Added Image upload to API

--- API/Program.cs
+++ API/Program.cs
@@ -1,15 +1,22 @@
 using API.Middleware;
+
 using Application.Activities.Queries;
 using Application.Activities.Validators;
 using Application.Core;
 using Application.Interfaces;
+
 using Domain;
+
 using FluentValidation;
+
+using Infrastructure.Photos;
 using Infrastructure.Security;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
+
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -34,6 +41,7 @@ builder.Services.AddMediatR(x =>
 });
 
 builder.Services.AddScoped<IUserAccessor, UserAccessor>();
+builder.Services.AddScoped<IPhotoService, PhotoService>();
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 builder.Services.AddValidatorsFromAssemblyContaining<CreateActivityValidator>();
 builder.Services.AddTransient<ExceptionMiddleware>();
@@ -51,6 +59,7 @@ builder.Services.AddAuthorization(opt =>
     });
 });
 builder.Services.AddTransient<IAuthorizationHandler, IsHostRequirementHandler>();
+builder.Services.Configure<CloudinarySettings>(builder.Configuration.GetSection("CloudinarySettings"));
 
 var app = builder.Build();
 
--- Infrastructure/Security/UserAccessor.cs
+++ Infrastructure/Security/UserAccessor.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using Application.Interfaces;
 using Domain;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Infrastructure.Security;
@@ -12,12 +13,22 @@ public class UserAccessor(IHttpContextAccessor httpContextAccessor, AppDbContext
     public async Task<User> GetUserAsync()
     {
         return await dbContext.Users.FindAsync(GetUserId())
-        ?? throw new UnauthorizedAccessException("No user is logged in");
+            ?? throw new UnauthorizedAccessException("No user is logged in");
     }
 
     public string GetUserId()
     {
         return httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)
-        ?? throw new Exception("No user found");
+            ?? throw new Exception("No user found");
+    }
+
+    public async Task<User> GetUserWithPhotosAsync()
+    {
+        var userId = GetUserId();
+
+        return await dbContext.Users
+        .Include(x => x.Photos)
+        .FirstOrDefaultAsync(x => x.Id == userId)
+            ?? throw new UnauthorizedAccessException("No user is logged in");
     }
 }
