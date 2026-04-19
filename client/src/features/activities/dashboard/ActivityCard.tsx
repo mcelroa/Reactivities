19d501b3e58a2773ec2dc0d63353223a6c8c1bf5
Adam McElroy <adam.mcelroy15@gmail.com>
2026-04-19 22:52:00 +0100

    Added client image upload

--- client/src/features/activities/dashboard/ActivityCard.tsx
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -1,114 +1,116 @@
 import { AccessTime, Place } from "@mui/icons-material";
 import {
-    Avatar,
-    Box,
-    Button,
-    Card,
-    CardContent,
-    CardHeader,
-    Chip,
-    Divider,
-    Typography,
+   Avatar,
+   Box,
+   Button,
+   Card,
+   CardContent,
+   CardHeader,
+   Chip,
+   Divider,
+   Typography,
 } from "@mui/material";
 import { Link } from "react-router";
 import { formatDate } from "../../../lib/util/util";
 import AvatarPopover from "../../../app/shared/components/AvatarPopover";
 
 type Props = {
-    activity: Activity;
+   activity: Activity;
 };
 
 export default function ActivityCard({ activity }: Props) {
-    const label = activity.isHost ? "You are hosting" : "You are going";
-    const color = activity.isHost
-        ? "secondary"
-        : activity.isGoing
-          ? "warning"
-          : "default";
+   const label = activity.isHost ? "You are hosting" : "You are going";
+   const color = activity.isHost
+      ? "secondary"
+      : activity.isGoing
+        ? "warning"
+        : "default";
 
-    return (
-        <Card elevation={3}>
-            <Box
-                display="flex"
-                justifyContent="space-between"
-                alignItems="center"
-            >
-                <CardHeader
-                    avatar={<Avatar sx={{ height: 80, width: 80 }} />}
-                    title={activity.title}
-                    titleTypographyProps={{
-                        fontWeight: "bold",
-                        fontSize: 20,
-                    }}
-                    subheader={
-                        <>
-                            Hosted by{" "}
-                            <Link to={`/profiles/${activity.hostId}`}>
-                                {activity.hostDisplayName}
-                            </Link>
-                        </>
-                    }
-                />
-                <Box display="flex" flexDirection="column" gap={2} mr={2}>
-                    {(activity.isHost || activity.isGoing) && (
-                        <Chip
-                            variant="outlined"
-                            label={label}
-                            color={color}
-                            sx={{ borderRadius: 2 }}
-                        />
-                    )}
-                    {activity.isCancelled && (
-                        <Chip
-                            label="Cancelled"
-                            color="error"
-                            sx={{ borderRadius: 2 }}
-                        />
-                    )}
-                </Box>
+   return (
+      <Card elevation={3}>
+         <Box display="flex" justifyContent="space-between" alignItems="center">
+            <CardHeader
+               avatar={
+                  <Avatar
+                     src={activity.hostImageUrl}
+                     sx={{ height: 80, width: 80 }}
+                     alt="image of host"
+                  />
+               }
+               title={activity.title}
+               titleTypographyProps={{
+                  fontWeight: "bold",
+                  fontSize: 20,
+               }}
+               subheader={
+                  <>
+                     Hosted by{" "}
+                     <Link to={`/profiles/${activity.hostId}`}>
+                        {activity.hostDisplayName}
+                     </Link>
+                  </>
+               }
+            />
+            <Box display="flex" flexDirection="column" gap={2} mr={2}>
+               {(activity.isHost || activity.isGoing) && (
+                  <Chip
+                     variant="outlined"
+                     label={label}
+                     color={color}
+                     sx={{ borderRadius: 2 }}
+                  />
+               )}
+               {activity.isCancelled && (
+                  <Chip
+                     label="Cancelled"
+                     color="error"
+                     sx={{ borderRadius: 2 }}
+                  />
+               )}
             </Box>
+         </Box>
 
-            <Divider sx={{ mb: 3 }} />
+         <Divider sx={{ mb: 3 }} />
 
-            <CardContent sx={{ mb: 0 }}>
-                <Box display="flex" alignItems="center" mb={2} px={2}>
-                    <Box display="flex" flexGrow={0} alignItems="center">
-                        <AccessTime sx={{ mr: 1 }} />
-                        <Typography variant="body2" noWrap>
-                            {formatDate(activity.date)}
-                        </Typography>
-                    </Box>
+         <CardContent sx={{ mb: 0 }}>
+            <Box display="flex" alignItems="center" mb={2} px={2}>
+               <Box display="flex" flexGrow={0} alignItems="center">
+                  <AccessTime sx={{ mr: 1 }} />
+                  <Typography variant="body2" noWrap>
+                     {formatDate(activity.date)}
+                  </Typography>
+               </Box>
 
-                    <Place sx={{ ml: 3, mr: 1 }} />
-                    <Typography variant="body2">{activity.venue}</Typography>
-                </Box>
-                <Divider />
-                <Box
-                    display="flex"
-                    gap={2}
-                    sx={{ backgroundColor: "grey.200", py: 3, pl: 3 }}
-                >
-                    {activity.attendees.map((att) => (
-                        <AvatarPopover profile={att} key={att.id} />
-                    ))}
-                </Box>
-            </CardContent>
-            <CardContent sx={{ pb: 2 }}>
-                <Typography variant="body2">{activity.description}</Typography>
-                <Button
-                    component={Link}
-                    to={`/activities/${activity.id}`}
-                    size="medium"
-                    variant="contained"
-                    sx={{
-                        display: "flex",
-                        justifySelf: "self-end",
-                        borderRadius: 3,
-                    }}
-                >
-                    View
-                </Button>
-            </CardContent>
-        </Card>
-    );
+               <Place sx={{ ml: 3, mr: 1 }} />
+               <Typography variant="body2">{activity.venue}</Typography>
+            </Box>
+            <Divider />
+            <Box
+               display="flex"
+               gap={2}
+               sx={{ backgroundColor: "grey.200", py: 3, pl: 3 }}
+            >
+               {activity.attendees.map((att) => (
+                  <AvatarPopover profile={att} key={att.id} />
+               ))}
+            </Box>
+         </CardContent>
+         <CardContent sx={{ pb: 2 }}>
+            <Typography variant="body2">{activity.description}</Typography>
+            <Button
+               component={Link}
+               to={`/activities/${activity.id}`}
+               size="medium"
+               variant="contained"
+               sx={{
+                  display: "flex",
+                  justifySelf: "self-end",
+                  borderRadius: 3,
+               }}
+            >
+               View
+            </Button>
+         </CardContent>
+      </Card>
+   );
 }
--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -4,150 +4,156 @@ import { useLocation } from "react-router";
 import { useAccount } from "./useAccount";
 
 export const useActivities = (id?: string) => {
-    const queryClient = useQueryClient();
-    const { currentUser } = useAccount();
-    const location = useLocation();
+   const queryClient = useQueryClient();
+   const { currentUser } = useAccount();
+   const location = useLocation();
 
-    const { data: activities, isLoading } = useQuery({
-        queryKey: ["activities"],
-        queryFn: async () => {
-            const response = await agent.get<Activity[]>("/activities");
-            return response.data;
-        },
-        enabled: !id && location.pathname === "/activities" && !!currentUser,
-        select: (data) => {
-            return data.map((activity) => {
-                return {
-                    ...activity,
-                    isHost: currentUser?.id === activity.hostId,
-                    isGoing: activity.attendees.some(
-                        (x) => x.id === currentUser?.id,
-                    ),
-                };
-            });
-        },
-    });
-
-    const { data: activity, isLoading: isLoadingActivity } = useQuery({
-        queryKey: ["activities", id],
-        queryFn: async () => {
-            const response = await agent.get<Activity>(`/activities/${id}`);
-            return response.data;
-        },
-        enabled: !!id && !!currentUser,
-        select: (data) => {
-            return {
-                ...data,
-                isHost: currentUser?.id === data.hostId,
-                isGoing: data.attendees.some((x) => x.id === currentUser?.id),
-            };
-        },
-    });
-
-    const updateActivity = useMutation({
-        mutationFn: async (activity: Activity) => {
-            await agent.put("/activities", activity);
-        },
-        onSuccess: async () => {
-            await queryClient.invalidateQueries({
-                queryKey: ["activities"],
-            });
-        },
-    });
-
-    const createActivity = useMutation({
-        mutationFn: async (activity: Activity) => {
-            const response = await agent.post("/activities", activity);
-            return response.data;
-        },
-        onSuccess: async () => {
-            await queryClient.invalidateQueries({
-                queryKey: ["activities"],
-            });
-        },
-    });
-
-    const deleteActivity = useMutation({
-        mutationFn: async (id: string) => {
-            await agent.delete(`/activities/${id}`);
-        },
-        onSuccess: async () => {
-            await queryClient.invalidateQueries({
-                queryKey: ["activities"],
-            });
-        },
-    });
-
-    const updateAttendance = useMutation({
-        mutationFn: async (id: string) => {
-            await agent.post(`/activities/${id}/attend`);
-        },
-        onMutate: async (activityId: string) => {
-            await queryClient.cancelQueries({
-                queryKey: ["activities", activityId],
-            });
-
-            const prevActivity = queryClient.getQueryData<Activity>([
-                "activities",
-                activityId,
-            ]);
-
-            queryClient.setQueryData<Activity>(
-                ["activities", activityId],
-                (oldActivity) => {
-                    if (!oldActivity || !currentUser) {
-                        return oldActivity;
-                    }
-
-                    const isHost = oldActivity.hostId === currentUser.id;
-                    const isAttending = oldActivity.attendees.some(
-                        (x) => x.id === currentUser.id,
-                    );
-
-                    return {
-                        ...oldActivity,
-                        isCancelled: isHost
-                            ? !oldActivity.isCancelled
-                            : oldActivity.isCancelled,
-                        attendees: isAttending
-                            ? isHost
-                                ? oldActivity.attendees
-                                : oldActivity.attendees.filter(
-                                      (x) => x.id !== currentUser.id,
-                                  )
-                            : [
-                                  ...oldActivity.attendees,
-                                  {
-                                      id: currentUser.id,
-                                      displayName: currentUser.displayName,
-                                      imageUrl: currentUser.imageUrl,
-                                  },
-                              ],
-                    };
-                },
+   const { data: activities, isLoading } = useQuery({
+      queryKey: ["activities"],
+      queryFn: async () => {
+         const response = await agent.get<Activity[]>("/activities");
+         return response.data;
+      },
+      enabled: !id && location.pathname === "/activities" && !!currentUser,
+      select: (data) => {
+         return data.map((activity) => {
+            const host = activity.attendees.find(
+               (x) => x.id === activity.hostId,
             );
+            return {
+               ...activity,
+               isHost: currentUser?.id === activity.hostId,
+               isGoing: activity.attendees.some(
+                  (x) => x.id === currentUser?.id,
+               ),
+               hostImageUrl: host?.imageUrl,
+            };
+         });
+      },
+   });
 
-            return { prevActivity };
-        },
-        onError: (error, activityId, context) => {
-            console.log(error);
-            if (context?.prevActivity) {
-                queryClient.setQueryData(
-                    ["activities", activityId],
-                    context.prevActivity,
-                );
-            }
-        },
-    });
+   const { data: activity, isLoading: isLoadingActivity } = useQuery({
+      queryKey: ["activities", id],
+      queryFn: async () => {
+         const response = await agent.get<Activity>(`/activities/${id}`);
+         return response.data;
+      },
+      enabled: !!id && !!currentUser,
+      select: (data) => {
+         const host = data.attendees.find((x) => x.id === data.hostId);
+         return {
+            ...data,
+            isHost: currentUser?.id === data.hostId,
+            isGoing: data.attendees.some((x) => x.id === currentUser?.id),
+            hostImageUrl: host?.imageUrl,
+         };
+      },
+   });
 
-    return {
-        activities,
-        isLoading,
-        updateActivity,
-        createActivity,
-        deleteActivity,
-        activity,
-        isLoadingActivity,
-        updateAttendance,
-    };
+   const updateActivity = useMutation({
+      mutationFn: async (activity: Activity) => {
+         await agent.put("/activities", activity);
+      },
+      onSuccess: async () => {
+         await queryClient.invalidateQueries({
+            queryKey: ["activities"],
+         });
+      },
+   });
+
+   const createActivity = useMutation({
+      mutationFn: async (activity: Activity) => {
+         const response = await agent.post("/activities", activity);
+         return response.data;
+      },
+      onSuccess: async () => {
+         await queryClient.invalidateQueries({
+            queryKey: ["activities"],
+         });
+      },
+   });
+
+   const deleteActivity = useMutation({
+      mutationFn: async (id: string) => {
+         await agent.delete(`/activities/${id}`);
+      },
+      onSuccess: async () => {
+         await queryClient.invalidateQueries({
+            queryKey: ["activities"],
+         });
+      },
+   });
+
+   const updateAttendance = useMutation({
+      mutationFn: async (id: string) => {
+         await agent.post(`/activities/${id}/attend`);
+      },
+      onMutate: async (activityId: string) => {
+         await queryClient.cancelQueries({
+            queryKey: ["activities", activityId],
+         });
+
+         const prevActivity = queryClient.getQueryData<Activity>([
+            "activities",
+            activityId,
+         ]);
+
+         queryClient.setQueryData<Activity>(
+            ["activities", activityId],
+            (oldActivity) => {
+               if (!oldActivity || !currentUser) {
+                  return oldActivity;
+               }
+
+               const isHost = oldActivity.hostId === currentUser.id;
+               const isAttending = oldActivity.attendees.some(
+                  (x) => x.id === currentUser.id,
+               );
+
+               return {
+                  ...oldActivity,
+                  isCancelled: isHost
+                     ? !oldActivity.isCancelled
+                     : oldActivity.isCancelled,
+                  attendees: isAttending
+                     ? isHost
+                        ? oldActivity.attendees
+                        : oldActivity.attendees.filter(
+                             (x) => x.id !== currentUser.id,
+                          )
+                     : [
+                          ...oldActivity.attendees,
+                          {
+                             id: currentUser.id,
+                             displayName: currentUser.displayName,
+                             imageUrl: currentUser.imageUrl,
+                          },
+                       ],
+               };
+            },
+         );
+
+         return { prevActivity };
+      },
+      onError: (error, activityId, context) => {
+         console.log(error);
+         if (context?.prevActivity) {
+            queryClient.setQueryData(
+               ["activities", activityId],
+               context.prevActivity,
+            );
+         }
+      },
+   });
+
+   return {
+      activities,
+      isLoading,
+      updateActivity,
+      createActivity,
+      deleteActivity,
+      activity,
+      isLoadingActivity,
+      updateAttendance,
+   };
 };
