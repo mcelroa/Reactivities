3646e6d5ccb3ef2cc5fb1c74fc22d1791747b299
Adam McElroy <adam.mcelroy15@gmail.com>
2026-04-25 13:09:07 +0100

    Added paging, sorting and filtering

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,22 +4,22 @@ import { Outlet, ScrollRestoration, useLocation } from "react-router";
 import HomePage from "../../features/home/HomePage";
 
 export default function App() {
-    const location = useLocation();
+   const location = useLocation();
 
-    return (
-        <Box sx={{ bgcolor: "#eeeeee", minHeight: "100vh" }}>
-            <ScrollRestoration />
-            <CssBaseline />
-            {location.pathname === "/" ? (
-                <HomePage />
-            ) : (
-                <>
-                    <NavBar />
-                    <Container maxWidth="xl" sx={{ mt: 3 }}>
-                        <Outlet />
-                    </Container>
-                </>
-            )}
-        </Box>
-    );
+   return (
+      <Box sx={{ bgcolor: "#eeeeee", minHeight: "100vh" }}>
+         <ScrollRestoration />
+         <CssBaseline />
+         {location.pathname === "/" ? (
+            <HomePage />
+         ) : (
+            <>
+               <NavBar />
+               <Container maxWidth="xl" sx={{ pt: 14 }}>
+                  <Outlet />
+               </Container>
+            </>
+         )}
+      </Box>
+   );
 }
--- client/src/features/activities/dashboard/ActivityFilters.tsx
+++ client/src/features/activities/dashboard/ActivityFilters.tsx
@@ -9,8 +9,13 @@ import {
 } from "@mui/material";
 import "react-calendar/dist/Calendar.css";
 import { Calendar } from "react-calendar";
+import { useStore } from "../../../lib/hooks/useStore";
+import { observer } from "mobx-react-lite";
 
-export default function ActivityFilters() {
+const ActivityFilters = observer(function ActivityFilters() {
+   const {
+      activityStore: { setFilter, setStartDate, filter, startDate },
+   } = useStore();
    return (
       <Box
          sx={{
@@ -35,13 +40,22 @@ export default function ActivityFilters() {
                   Filters
                </Typography>
                <MenuList>
-                  <MenuItem>
+                  <MenuItem
+                     selected={filter === "all"}
+                     onClick={() => setFilter("all")}
+                  >
                      <ListItemText primary="All events" />
                   </MenuItem>
-                  <MenuItem>
+                  <MenuItem
+                     selected={filter === "isGoing"}
+                     onClick={() => setFilter("isGoing")}
+                  >
                      <ListItemText primary="I'm going" />
                   </MenuItem>
-                  <MenuItem>
+                  <MenuItem
+                     selected={filter === "isHost"}
+                     onClick={() => setFilter("isHost")}
+                  >
                      <ListItemText primary="I'm hosting" />
                   </MenuItem>
                </MenuList>
@@ -60,8 +74,13 @@ export default function ActivityFilters() {
                <Event sx={{ mr: 1 }} />
                Select Date
             </Typography>
-            <Calendar />
+            <Calendar
+               value={startDate}
+               onChange={(date) => setStartDate(date as Date)}
+            />
          </Box>
       </Box>
    );
-}
+});
+
+export default ActivityFilters;
